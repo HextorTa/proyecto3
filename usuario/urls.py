fae62e045639b0269472e169e66d8cfb02354951
hector <hectortapia012@gmail.com>
2024-07-05 02:10:14 -0400

    epico

--- usuario/urls.py
+++ usuario/urls.py
@@ -4,8 +4,8 @@ from .views import Pagina1, Pagina2, Pagina3, Pagina4, Pagina5, Pagina6
 urlpatterns = [
     path('', Pagina1, name= 'Pagina1'),
     path('Pagina2/', Pagina2, name= 'Pagina2'),
-    path('Pagina3/', Pagina2, name= 'Pagina3'),
-    path('Pagina4/', Pagina2, name= 'Pagina4'),
-    path('Pagina5/', Pagina2, name= 'Pagina5'),
-    path('Pagina6/', Pagina2, name= 'Pagina6'),
+    path('Pagina3/', Pagina3, name= 'Pagina3'),
+    path('Pagina4/', Pagina4, name= 'Pagina4'),
+    path('Pagina5/', Pagina5, name= 'Pagina5'),
+    path('Pagina6/', Pagina6, name= 'Pagina6'),
 ]
